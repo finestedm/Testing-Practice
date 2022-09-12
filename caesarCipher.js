abcdad63b977fcba916c49c45f136651579f4e33
Paweł <finestedm@gmail.com>
2022-09-12 22:51:32 +0200

    caesarCipher added and tested

--- caesarCipher.js
+++ caesarCipher.js
@@ -3,14 +3,38 @@ const alphabetReversed = 'ZYXWVUTSRQPONMLKJIHGFEDCBA';
 
 export function caesarCipher(string) {
 
-    var reversedString = ''
+    var reversedString = '' // declare variable to which we will add ciphered letters
 
-    string.split('').forEach(letter => {
-        const searchedLetter = alphabet.indexOf(letter);
-        const searchedLetterReversed = alphabetReversed[searchedLetter];
-        reversedString += searchedLetterReversed
+    if (typeof string !== 'string' && typeof string !== 'number') { // accept only string or number
+        return 'This is not riiiight';
+    }
+
+    string.split('').forEach(letter => {    // split string into separate characters
+        if (isNumeric(letter)) {            // if a character is number leave it as it is
+            reversedString += letter;
+        } else if (letter === ' ') {        // if a character is space leave it as it is 
+            reversedString += letter
+        } else if (alphabet.includes(`${letter.toUpperCase()}`)) {  // if the character is uppercase we can search for it
+            if (letter === letter.toUpperCase()) {
+                const searchedLetter = alphabet.indexOf(letter);
+                const searchedLetterReversed = alphabetReversed[searchedLetter];
+                reversedString += searchedLetterReversed
+            } else if (letter === letter.toLowerCase()) { // if the character is lowercase we can search for it only after uppercasing it
+                const searchedLetter = alphabet.indexOf(letter.toUpperCase());
+                const searchedLetterReversed = alphabetReversed[searchedLetter];
+                reversedString += searchedLetterReversed.toLowerCase()      // switch the ciphered character to lowercase
+            }
+        } else {
+            reversedString += letter    // if this is any other sign we can just add it
+        }
     });
 
     return reversedString
 
 }
+
+function isNumeric(n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
+}
+
+// console.log(caesarCipher('PIES1'))
--- caesarCipher.test.js
+++ caesarCipher.test.js
@@ -1,5 +1,21 @@
 import { caesarCipher } from "./caesarCipher";
 
 test('casearCipher with all uppercase', () => {
-    expect(caesarCipher('PIES')).toBe('KRVH');
+    expect(caesarCipher('PIES1')).toBe('KRVH1');
+});
+
+test('casearCipher with mixed case', () => {
+    expect(caesarCipher('pIeS 1')).toBe('kRvH 1');
+});
+
+test('casearCipher with number only', () => {
+    expect(caesarCipher('123')).toBe('123');
+});
+
+test('casearCipher with float', () => {
+    expect(caesarCipher('.123')).toBe('.123');
+});
+
+test('casearCipher with string and float', () => {
+    expect(caesarCipher('chleb .123')).toBe('xsovy .123');
 });
